cbb3b473e4858ed680dc258143964b7d5be38bce
Alex Cipollone <alex.cipollone10@gmail.com>
2023-10-31 14:30:19 -0400

    completed 14.4 - 14.6

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,31 @@
+import { getAuth } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+
 function Profile() {
+  const auth = getAuth();
+  const [formData, setformData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+
+  const { name, email } = formData;
+
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    auth.signOut();
+    navigate("/");
+  };
+
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button type="button" className="logOut" onClick={onLogout}>
+          Logout
+        </button>
+      </header>
     </div>
   );
 }
