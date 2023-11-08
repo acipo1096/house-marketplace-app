9bfef9f13791b05568fe989596e3cf4369be4d95
Alex Cipollone <alex.cipollone10@gmail.com>
2023-11-08 11:38:02 -0500

    fixed errors in this section

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,16 +1,17 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import { useAuthStatus } from '../hooks/useAuthStatus';
-import { Spinner } from './Spinner'
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuthStatus } from "../hooks/useAuthStatus";
+import Spinner from "./Spinner";
 
 const PrivateRoute = () => {
+  const { loggedIn, checkingStatus } = useAuthStatus();
 
-    const { loggedIn, checkingStatus } = useAuthStatus();
+  if (checkingStatus) {
+    return <Spinner />;
+  }
 
-    if(checkingStatus) {
-        return <Spinner />
-    }
+  return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
+  // Video 8 End - should now be able to log in and check with Firebase
+  // Video 8 - 3:34 - should get redirect to sign in page
+};
 
-    return loggedIn ? <Outlet /> : <Navigate to='/sign-in' />
-    // Video 8 End - should now be able to log in and check with Firebase
-    // Video 8 - 3:34 - should get redirect to sign in page
-}
+export default PrivateRoute;
