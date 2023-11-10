09c2fe73aef04bc17eb781acc2b1b2defccef082
Alex Cipollone <alex.cipollone10@gmail.com>
2023-11-10 09:42:37 -0500

    completed 'Create Single Listing'

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -8,12 +8,25 @@ import shareIcon from "../assets/svg/shareIcon.svg";
 
 function Listing() {
   const [listing, setListing] = useState(null);
-  const [loading, setLoading] = useState(null);
-  const [shareLinkCopied, setShareLinkCopied] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
 
   const navigate = useNavigate();
   const params = useParams();
   const auth = getAuth();
+
+  useEffect(() => {
+    const fetchListing = async () => {
+      const docRef = doc(db, "listings", params.listingId);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        console.log(docSnap.data());
+        setListing(docSnap.data());
+        setLoading(false);
+      }
+    };
+  }, [navigate, params.listingId]);
   return <div>LISTING</div>;
 }
 
